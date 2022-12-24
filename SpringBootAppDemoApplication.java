265dd558754b14d01be7af111fc318f2ccf0a0d3
zohrehshahabi <115090132+zohrehshahabi@users.noreply.github.com>
2022-12-24 10:07:58 +0330

    Add files via upload

---
+++ SpringBootAppDemoApplication.java
@@ -0,0 +1,22 @@
+package com.exercise.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringBootAppDemoApplication {
+private static final Logger log=  LoggerFactory.getLogger(SpringBootAppDemoApplication.class);
+	public static void main(String[] args) {
+		ConfigurableApplicationContext  applicationContext =SpringApplication
+				.run(SpringBootAppDemoApplication.class,args);
+		DbConfiguration dbConfiguration=applicationContext.getBean(DbConfiguration.class);
+		log.info(dbConfiguration.toString());
+
+
+
+	}
+
+}
